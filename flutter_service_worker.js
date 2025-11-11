732f927e88ec5c34b685e255a3a62b3ada59ee62
Anastasiya <213661989+AnastasiyaNikolaeva333@users.noreply.github.com>
2025-11-11 20:30:02 +0300

    playy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "44c9e67a7ce081eef067f6300c21d735
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "938e7fabaeb4a3c62ab2ea788f4f2471",
+"flutter_bootstrap.js": "a2f08a4c2666271d1d91413443fae7dc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
